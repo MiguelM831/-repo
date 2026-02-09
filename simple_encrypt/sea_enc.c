57ef860a67099c7e19de776712e00e7e656ac6b9
migue <miguel.martinez.r831@gmail.com>
2026-02-08 19:44:41 -0800

    Changed the "Missing Command Line" output to one line to make it neater.

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if(argc == 1) {
-        printf("Expected Exit Code: 1\nUsage: %s", argv[0]);
-        printf(" KEY_IN_HEX\n");
-    } else {
+    if(argc == 1) { //Missing command line output and exit
+        printf("Expected Exit Code: 1\nUsage: %s KEY_IN_HEX\n", argv[0]);
+    } else { 
         printf("Expected Exit Code: 0\n");
     }
     return 0;
