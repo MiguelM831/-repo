b79e5c134aad845a48599cfb30b57a89a8e698bf
migue <miguel.martinez.r831@gmail.com>
2026-02-08 18:41:49 -0800

    Check for command line input and missing value output

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-
+int main(int argc, char *argv[]) {
+    if(argc == 1) {
+        printf("Expected Exit Code: 1\nUsage: %s", argv[0]);
+        printf(" KEY_IN_HEX\n");
+    } else {
+        printf("Expected Exit Code: 0\n");
+    }
     return 0;
 }
