687ad01488a48c680481e9b69cb1bbafb08b0313
migue <miguel.martinez.r831@gmail.com>
2026-02-08 21:59:19 -0800

    Got the key value from command line and output it for testing purposes.

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
     if(argc == 1) { //Missing command line output and exit
         printf("Expected Exit Code: 1\nUsage: %s KEY_IN_HEX\n", argv[0]);
     } else { 
-        printf("Expected Exit Code: 0\n");
+        long key;
+        key = strtol(argv[1], NULL, 16); //Get key number value from command line
+        
+        printf("Expected Exit Code: 0\n%ld\n", key);
     }
     return 0;
 }
