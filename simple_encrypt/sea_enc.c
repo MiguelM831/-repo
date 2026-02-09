1b64889b816ace6683d567654ccccc2c8d804e41
migue <miguel.martinez.r831@gmail.com>
2026-02-09 09:38:49 -0800

    Managed to test a XOR on each input with the key but need to change the order of some outputs.

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -3,14 +3,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define MAX_SIZE 512
+
 int main(int argc, char *argv[]) {
     if(argc == 1) { //Missing command line output and exit
         printf("Expected Exit Code: 1\nUsage: %s KEY_IN_HEX\n", argv[0]);
     } else { 
         long key;
+        char inp[MAX_SIZE];
+        ssize_t bytesR;
+        char c;
         key = strtol(argv[1], NULL, 16); //Get key number value from command line
-        
-        printf("Expected Exit Code: 0\n%ld\n", key);
+        bytesR = read(0, inp, MAX_SIZE);
+            for(int i = 0; i <= bytesR; i++) {
+                c = inp[i];
+                c ^= key; //XOR the character with the key
+                printf("%c", c);
+            }
+        printf("\nExpected Exit Code: 0\n%ld\n", key);
     }
     return 0;
 }
