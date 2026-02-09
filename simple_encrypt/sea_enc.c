51e7b66c49404478110eae9332f3622c5d139071
migue <miguel.martinez.r831@gmail.com>
2026-02-09 11:53:14 -0800

    Removed some unnecessary code and added comments for clarity.

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -10,24 +10,23 @@ int main(int argc, char *argv[]) {
     if(argc == 1) { //Missing command line output and exit
         printf("Expected Exit Code: 1\nUsage: %s KEY_IN_HEX\n", argv[0]);
     } else { 
-        uint16_t key;
-        char inp[MAX_SIZE];
-        unsigned char c;
+        uint16_t key; //Encrypt key
+        char inp[MAX_SIZE]; //Character for input
+        unsigned char c; //Character to hold the encrypted value
         key = strtol(argv[1], NULL, 16); //Get key number value from command line
-        ssize_t bytesR = read(0, inp, MAX_SIZE);
+        ssize_t bytesR = read(0, inp, MAX_SIZE); //Receive input from user
         printf("Expected Exit Code: 0\n");
             for(ssize_t i = 0; i < bytesR-1; i++) {
-                c = (unsigned char)inp[i] ^ (unsigned char)key;
-                c &= 0xFF;
-                printf("%X", c);
-                key = (key << 1) | (key >> 15);
-                if((c & 0x80) == 0) {
-                    key *= 257;
-                }else {
-                    key += 1;
-                    key *= 257;
+                c = (unsigned char)inp[i] ^ (unsigned char)key; //XOR input and key and store in c
+                c &= 0xFF; //Limit to 2 bytes in hex
+                if(inp[i] == ' ') { //Check if input is empty
+                    printf("\n");
+                } else {
+                    printf("%X", c); //Output c in hex
+                    key = (key << 1) | (key >> 15); //Rotate key left 1
+                    key *= 257; //Multiply key by 257
+                    if((i+1)%40 == 0 && i != 0) printf("\n"); //Print newline after 40 chars
                 }
-                if((i+1)%40 == 0 && i != 0) printf("\n");
             }
             printf("\n");
     }
