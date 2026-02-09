7b39bd21b9b3dd8aee2545a37e336c4c0377ad81
migue <miguel.martinez.r831@gmail.com>
2026-02-09 11:13:46 -0800

    Started and fixed the left shift or "Rotate left 1" operator

--- simple_encrypt/sea_enc.c
+++ simple_encrypt/sea_enc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -9,18 +10,23 @@ int main(int argc, char *argv[]) {
     if(argc == 1) { //Missing command line output and exit
         printf("Expected Exit Code: 1\nUsage: %s KEY_IN_HEX\n", argv[0]);
     } else { 
-        long key;
+        uint16_t key;
         char inp[MAX_SIZE];
-        ssize_t bytesR;
-        char c;
+        unsigned char c;
         key = strtol(argv[1], NULL, 16); //Get key number value from command line
-        bytesR = read(0, inp, MAX_SIZE);
-        printf("Expected Exit Code: 0\n");
-            for(int i = 0; i < bytesR-1; i++) {
-                c = inp[i];
-                c ^= key; //XOR the character with the key
+        ssize_t bytesR = read(0, inp, MAX_SIZE);
+        printf("Expected Exit Code: 0\n%zd\n", bytesR);
+            for(ssize_t i = 0; i < bytesR-1; i++) {
+                c = (unsigned char)inp[i] ^ (unsigned char)key;
                 c &= 0xFF;
                 printf("%X", c);
+                key = (key << 1) | (key >> 15);
+                if((c & 0x80) == 0) {
+                    key *= 257;
+                }else {
+                    key += 1;
+                    key *= 257;
+                }
             }
             printf("\n");
     }
