5cb7757087702268b2d410b8ac370b4c709bbc9d
migue <miguel.martinez.r831@gmail.com>
2026-02-11 15:13:23 -0800

    Created sea_break file for a decrypt code.

---
+++ hacker-decrypt/sea_break.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define MAX_SIZE 512
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
